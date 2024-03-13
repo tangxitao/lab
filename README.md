dbbab97418a3028d58792a8130a763db285da288
tangxitao <2682585843@qq.com>
2024-03-14 00:08:05 +0800

    666

--- README.md
+++ README.md
@@ -2,14 +2,22 @@
 
 每隔15s采集一次数据，在181,183,185上面
 
-数据指标
-cpu利用率cpu usage
-gpu利用率gpu usage
-gpu功率gpu power
-gpu温度gpu temp
-磁盘读写速率sda
+### 数据指标
+1. cpu利用率cpu usage
+2. gpu利用率gpu usage
+3. gpu功率gpu power
+4. gpu温度gpu temp
+5. 磁盘读写速率sda
 
+
+### 文件夹存放 
 normal文件夹存放着无故障时的训练数据，训练历时1m 44s
 
-cpufullload是往185节点注入1分钟左右的cpu满载故障，训练历时2m 3s
+cpufullload是往185节点注入75s的cpu满载故障，训练历时2m 3s
 
+gpu_burn是对185节点进行1分钟的GPU_burn，训练历时2m 14s。
+
+
+diskburn文件夹对185进行磁盘高负载故障90s，训练历时1m 46s，可视为无影响。
+
+netloss文件夹对185进行50%的丢包处理，训练历时1m42s，可视为无影响（在终端输出的信息中，一个节点丢包网络不好，ray会把任务分给网络好的节点，总时长基本不变）
--- normal/原始/1.py
+++ normal/原始/1.py
@@ -58,8 +58,22 @@ df3 = df3.rename(columns={'GPU 1.2': '183_GPU1_usage'})
 df3 = df3.rename(columns={'GPU 2.2': '183_GPU2_usage'})
 df3 = df3.rename(columns={'GPU 3.2': '183_GPU3_usage'})
 
+df4=pd.read_csv("GPU Framebuffer.csv")
+del df4["Time"]
+df4 = df4.rename(columns={'GPU 0': '181_GPU0_mem'})
+df4 = df4.rename(columns={'GPU 1': '181_GPU1_mem'})
+df4 = df4.rename(columns={'GPU 2': '181_GPU2_mem'})
+df4 = df4.rename(columns={'GPU 3': '181_GPU3_mem'})
+df4 = df4.rename(columns={'GPU 0.1': '185_GPU0_mem'})
+df4 = df4.rename(columns={'GPU 1.1': '185_GPU1_mem'})
+df4 = df4.rename(columns={'GPU 2.1': '185_GPU2_mem'})
+df4 = df4.rename(columns={'GPU 3.1': '185_GPU3_mem'})
+df4 = df4.rename(columns={'GPU 0.2': '183_GPU0_mem'})
+df4 = df4.rename(columns={'GPU 1.2': '183_GPU1_mem'})
+df4 = df4.rename(columns={'GPU 2.2': '183_GPU2_mem'})
+df4 = df4.rename(columns={'GPU 3.2': '183_GPU3_mem'})
 
-df=pd.concat([df,df1,df2,df3],axis=1)
+df=pd.concat([df,df1,df2,df3,df4],axis=1)
 
 df.to_csv("1.csv",index=False)
 print(df)
